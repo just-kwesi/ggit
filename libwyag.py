6b8a247cea791c47485c96148805b105310f3f28
Frederick Tetteh <kwesiossom@gmail.com>
2025-04-22 11:21:18 -0500

    added tree parsing

--- libwyag.py
+++ libwyag.py
@@ -198,6 +198,78 @@ class GitCommit(GitObject):
         return kvlm_serialize(self.kvlm)
     def init(self):
         self.kvlm = dict()
+
+class GitTreeLeaf(object):
+    def __init__(self,mode,path,sha):
+        self.mode = mode
+        self.path = path
+        self.sha = sha
+        
+class GitTree(GitObject):
+    fmt=b'tree'
+    
+    def deserialize(self, data):
+        self.items = tree_parse(data)
+        
+    def serialize(self):
+        return tree_serialize(self)
+    
+    def init(self):
+        self.items = list()
+
+
+def tree_parse_one(raw,start=0):
+    # find the space terminator of the mode
+    x = raw.find(b' ', start)
+    assert x-start == 5 or x-start==6
+    
+    # read the mode
+    mode = raw[start:x]
+    if len(mode) == 5:
+        # Normalize to six bytes
+        mode = b"0" + mode
+        
+    # Find the NULL terminator of the path
+    y = raw.find(b'\x00',x)
+    # and read the path
+    path = raw[x+1:y]
+    
+    # Read the SHA...
+    raw_sha = int.from_bytes(raw[y+1:y+21], 'big')
+    #  and convert it into an hex string, padded to 4o chars
+    # with zeros if needed
+    sha = format(raw_sha, "040x")
+    return y+21, GitTreeLeaf(mode,path.decode('utf8'),sha)
+
+def tree_parse(raw):
+    pos = 0
+    max = len(raw)
+    ret = list()
+    while pos < max:
+        pos, data = tree_parse_one(raw,pos)
+        ret.append(data)
+    return ret
+
+
+def tree_leaf_sort_key(leaf):
+    if leaf.mode.startswith(b"10"):
+        return leaf.path
+    else:
+        return leaf.path + "/"
+    
+def tree_serialize(obj):
+    obj.items.sort(key=tree_leaf_sort_key)
+    ret = b''
+    
+    for i in obj.items:
+        ret+=i.mode
+        ret+=b' '
+        ret+=i.path.encode("utf8")
+        ret+=b'\x00'
+        sha=int(i.sha,16)
+        ret+=sha.to_bytes(20,byteorder="big")
+    return ret
+    
         
 def object_read(repo,sha):
     """Read object sha from Git repository repo.  Return a
